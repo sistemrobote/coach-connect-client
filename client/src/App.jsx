6c41c6b2dd6925a4d8a782ab44697d2ddd403f62
sistemrobote <sistemrobote@gmail.com>
2025-05-07 23:08:31 -0400

    Initial commit stravaConnectApp

---
+++ client/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const clientID = import.meta.env.VITE_STRAVA_CLIENT_ID;
+
+function App() {
+  const [activities, setActivities] = useState([]);
+  const [userId, setuserId] = useState('');
+
+  const login = async () => {
+    window.location.href = `https://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=http://localhost:5050/auth/exchange_token&approval_prompt=force&scope=activity:read_all`;
+  };
+
+  const fetchLastActivities = async (userId) => {
+    try {
+      const res = await axios.get(`http://localhost:5050/activities?user_id=${userId}`);
+      // const res = await axios.get(`http://localhost:5050/lastactivities?user_id=${userId}`);
+      setActivities(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const user_id = params.get('user_id');
+    if (user_id) {
+      localStorage.setItem('strava_user_id', user_id);
+      setuserId(user_id);
+    }
+
+    if (window.location.pathname.includes('activities') || user_id) {
+      fetchLastActivities(user_id);
+    }
+  }, []);
+
+  const formatTime = (seconds) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}m ${secs}s`;
+  };
+
+  const formatPace = (seconds, meters) => {
+    const pace = seconds / (meters / 1000); // sec per km
+    const mins = Math.floor(pace / 60);
+    const secs = Math.round(pace % 60);
+    return `${mins}:${secs < 10 ? "0" : ""}${secs} /km`;
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      {!userId && <><h1>Strava Connect</h1>
+        <button onClick={login}>Connect to Strava</button></>}
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>
+            <div>
+              <h3>{activity.name}</h3>
+              <p><strong>Date:</strong> {new Date(activity.start_date_local).toLocaleString()}</p>
+              <p><strong>Distance:</strong> {(activity.distance / 1000).toFixed(2)} km</p>
+              <p><strong>Running Time:</strong> {formatTime(activity.moving_time)}</p>
+              <p><strong>Pace:</strong> {formatPace(activity.moving_time, activity.distance)}</p>
+              <hr />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ client/vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico'],
+      manifest: {
+        name: 'Strava Connect App',
+        short_name: 'StravaConnect',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#f4501e',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
