301cc276ac58e5754f91f4df86a7ee08bced32dc
sistemrobote <sistemrobote@gmail.com>
2025-08-13 17:27:14 -0400

    apiBaseUrl url fix

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,7 +3,7 @@ const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const Login = () => {
   const login = async () => {
-    window.location.href = `https://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${apiBaseUrl}/auth/exchange_token&approval_prompt=force&scope=activity:read_all`;
+    window.location.href = `https://www.strava.com/oauth/authorize?client_id=${clientID}&response_type=code&redirect_uri=${apiBaseUrl}auth/exchange_token&approval_prompt=force&scope=activity:read_all`;
   };
 
   return (
